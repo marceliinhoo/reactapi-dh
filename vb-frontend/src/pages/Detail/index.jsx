bcc5f90db12ac6ee72df94a5d905a91e68f6ebaa
Bianca Schoti <bia_schoti@hotmail.com>
2023-06-17 11:01:10 -0300

    Ajuste CSS

--- vb-frontend/src/pages/Detail/index.jsx
+++ vb-frontend/src/pages/Detail/index.jsx
@@ -4,10 +4,13 @@ import "./detail.css"
 import Header from "../../components/Header"
 import Footer from "../../components/Footer"
 import api from "../../services/api"
+import { getCookie } from "../../Utils"
+import { useNavigate } from "react-router-dom"
 
 const Detail = () => {
   const location = useLocation()
-
+  const navigate = useNavigate()
+  
   const [product, setProduct] = useState()
 
   useEffect(() => {
@@ -20,22 +23,26 @@ const Detail = () => {
   }
 
   const handleDelete = async () => {
-    const productDelete = await api.del(`/product/${location.state.id}`)
-    setProduct(productDelete.data)
+  try {
+  await api.delete(`/product/${location.state.id}`, { headers: {Authorization: getCookie('auth')}})
+  alert ('Seu produto foi excluido. Parabéns!')
+  navigate ('/product/nossoproduto')
 
-    console.log('DELETAR REGISTRO')
+}catch (error){
+  alert(error.response.data.error)
+}
   }
 
   return (
     <>
       <Header />
-
       {product ? (
               <div className="container products-detail">
               <div className="row">
 
               <div className="grup">
                 <div className="row">
+                <div className="col-12">
                   <div className="col-2 col-lg-8">
                     <img src={`http://localhost:3000/images/Produtos/${product.image}`} alt="product image" class="product-detail-img"/>
                   </div>
@@ -54,33 +61,37 @@ const Detail = () => {
                       <p className="product-description">
                       {product.description}
                       </p>
-                      <ul className="actions-list">
+                    </article>
+                    <ul className="actions-list">
                         <div clclassNameass="col-md-5" id="quantidade">
                       <input type="button" id="minus" value="-" />
                       <input id="quant" name="quant" className="text" size="1" value="1" type="text" maxlength="2" />
                       <input type="button" id="plus" value="+" />
                         </div>
-                        <Link to="/carrinho" type="button" className="btn bb"><p>Comprar</p></Link>
-                          <button type="submit"
-                           className="action-button delete"
-                            onClick={handleDelete}
-                           >
-                            Excluir Produto</button>
-                            <Link
-                            className="action-button edit"
-                            to="/product-update"
-                            state={{ id: product.id }}
-                            >
-                             Editar Produto
-                            </Link>
                       </ul>
-                    </article>
+                      <Link to="/carrinho" type="button" className="btn bb">Comprar</Link>
+                      <div className="Update-delete">
+                      <button
+                    className="action-button delete"
+                    onClick={handleDelete}
+                  >
+                    Deletar Produto
+                  </button>
+                      <Link
+                    className="action-button edit"
+                    to="/product-update"
+                    state={{ id: product.id }}
+                  >
+                    Editar Produto
+                  </Link>
+
+                  </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-
+          </div>
       ) : null}
 
       <Footer />
